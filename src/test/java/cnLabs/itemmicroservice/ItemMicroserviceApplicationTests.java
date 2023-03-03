9cb7d08c965138bc1b19a4b69eb9da727c2c21b4
Nathan Lively <60303904+LiveNathan@users.noreply.github.com>
2023-03-03 08:23:53 -0600

    connected to Eureka

---
+++ src/test/java/cnLabs/itemmicroservice/ItemMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package cnLabs.itemmicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItemMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
