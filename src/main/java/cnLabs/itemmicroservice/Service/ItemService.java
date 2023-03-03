db3b2a1b77b78a5ea7f95a33e7d3a760268114a3
Nathan Lively <60303904+LiveNathan@users.noreply.github.com>
2023-03-02 21:18:09 -0600

    second commit

---
+++ src/main/java/cnLabs/itemmicroservice/Service/ItemService.java
@@ -0,0 +1,41 @@
+package cnLabs.itemmicroservice.Service;
+
+import cnLabs.itemmicroservice.Model.Item;
+import cnLabs.itemmicroservice.Repo.ItemRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ItemService {
+
+    @Autowired
+    ItemRepository itemRepository;
+
+    public List<Item> getAllItems() {
+        return itemRepository.findAll();
+    }
+
+    public Item getItemById(Long id) {
+        Optional<Item> optional;
+        if ((optional = itemRepository.findById(id)).isEmpty()) {
+            return null;
+        } else {
+            return optional.get();
+        }
+    }
+
+    public Item insertNewItem(Item item) {
+        return itemRepository.save(item);
+    }
+
+    public Item updateItem(Item item) {
+        return itemRepository.save(item);
+    }
+
+    public void deleteItemById(Long id) {
+        itemRepository.deleteById(id);
+    }
+}
